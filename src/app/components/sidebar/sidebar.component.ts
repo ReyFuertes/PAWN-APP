db8b390e0dd6a1db61289862973e7e24e5a8d8bd
Rey Fuertes <rfuertes@cyrious.com>
2018-09-10 05:21:04 +0800

    sidebar anchor links changes

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,13 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, HostListener } from "@angular/core";
+declare var $: any;
 
 @Component({
-  selector: 'pa-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: [ './sidebar.component.scss' ]
+  selector: "pa-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"]
 })
 
-export class SidebarComponent implements OnInit {
-  constructor() { }
+export class SidebarComponent implements OnInit, AfterViewInit {
+  public sidebarLinks = [
+    {
+      text: "Dashboard",
+      icon: "fa-chart-pie",
+      route: "/dashboard"
+    },
+    {
+      text: "Accounts",
+      icon: "fa-user",
+      route: "/dashboard/accounts"
+    },
+    {
+      text: "Loans",
+      icon: "fa-gem",
+      route: "/dashboard/loans"
+    },
+    {
+      text: "Items",
+      icon: "fa-align-justify",
+      route: "/dashboard/items"
+    },
+    {
+      text: "Renewals",
+      icon: "fa-sync-alt",
+      route: "/dashboard/renewals"
+    },
+    {
+      text: "Redemptions",
+      icon: "fa-gift",
+      route: "/dashboard/redemptions"
+    }
+  ];
 
-  ngOnInit() { }
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  public gotoRoute(event: any): void {
+    let links = document.querySelectorAll(".router-link-item");
+
+    console.log(links);
+    links.forEach(link => {
+      link.classList.remove('active');
+    });
+
+    event.target.parentElement.classList.add('active');
+  }
+
+  ngAfterViewInit(): void {
+    let link = document.querySelectorAll(".router-link-item");
+    link[0].classList.add('active');
+  }
 }
