c0eb77341e1e682537e6430fedb10b223cff7cf0
Rey Fuertes <reynelfuertes@gmail.com>
2018-09-17 06:52:07 +0800

    addition adjustments

--- src/app/components/accounts/accounts.component.ts
+++ src/app/components/accounts/accounts.component.ts
@@ -50,6 +50,78 @@ export class AccountsComponent implements OnInit {
         fullname: "John Abbott",
         contact_number: "0933 9690 655",
         address: "Grand Residences Mabolo Cebu City"
+      },
+      {
+        account_id: "ACT-000000006",
+        fullname: "John Abbott",
+        contact_number: "0933 9690 655",
+        address: "Grand Residences Mabolo Cebu City"
+      },
+      {
+        account_id: "ACT-000000007",
+        fullname: "John Abbott",
+        contact_number: "0933 9690 655",
+        address: "Grand Residences Mabolo Cebu City"
+      },
+      {
+        account_id: "ACT-000000008",
+        fullname: "John Abbott",
+        contact_number: "0933 9690 655",
+        address: "Grand Residences Mabolo Cebu City"
+      },
+      {
+        account_id: "ACT-000000009",
+        fullname: "John Abbott",
+        contact_number: "0933 9690 655",
+        address: "Grand Residences Mabolo Cebu City"
+      },
+      {
+        account_id: "ACT-000000010",
+        fullname: "John Abbott",
+        contact_number: "0933 9690 655",
+        address: "Grand Residences Mabolo Cebu City"
+      },
+      {
+        account_id: "ACT-000000011",
+        fullname: "John Abbott",
+        contact_number: "0933 9690 655",
+        address: "Grand Residences Mabolo Cebu City"
+      },
+      {
+        account_id: "ACT-000000012",
+        fullname: "John Abbott",
+        contact_number: "0933 9690 655",
+        address: "Grand Residences Mabolo Cebu City"
+      },
+      {
+        account_id: "ACT-000000013",
+        fullname: "John Abbott",
+        contact_number: "0933 9690 655",
+        address: "Grand Residences Mabolo Cebu City"
+      },
+      {
+        account_id: "ACT-000000014",
+        fullname: "John Abbott",
+        contact_number: "0933 9690 655",
+        address: "Grand Residences Mabolo Cebu City"
+      },
+      {
+        account_id: "ACT-000000015",
+        fullname: "John Abbott",
+        contact_number: "0933 9690 655",
+        address: "Grand Residences Mabolo Cebu City"
+      },
+      {
+        account_id: "ACT-000000016",
+        fullname: "John Abbott",
+        contact_number: "0933 9690 655",
+        address: "Grand Residences Mabolo Cebu City"
+      },
+      {
+        account_id: "ACT-000000017",
+        fullname: "John Abbott",
+        contact_number: "0933 9690 655",
+        address: "Grand Residences Mabolo Cebu City"
       }
     ];
   }
